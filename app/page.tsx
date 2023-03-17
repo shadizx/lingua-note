10b722988cfa2f3c40f9c138646e0e75c2a7c0c0
Shadi Zoldjalali <shadizold@gmail.com>
2023-03-17 12:13:52 -0700

    Add hero page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,21 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <main className="text-6xl font-medium">Hello World</main>;
+  return (
+    <main className="h-screen flex flex-wrap items-center justify-center text-slate-300">
+      <div className="flex flex-wrap w-full text-center p-0 m-0 justify-center">
+        <div className="w-full p-4">
+          <p className="text-6xl font-medium w-full p-4">
+            Revolutionize your video learning with AI-powered notes.
+          </p>
+        </div>
+        <Link
+          className="border rounded-sm px-8 py-2 text-xl font-medium"
+          href={"/home"}
+        >
+          <p>Try now</p>
+        </Link>
+      </div>
+    </main>
+  );
 }
